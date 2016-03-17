5a665ed4fa309bde48cb1c82119dedfa60fca0ef
hassanila97 <hassanila97@gmail.com>
2016-03-17 08:01:16 +0100

    Set amount to 100
    Set amount to 100 and change to default name

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
 
-amount: 200, // MAX Amount of bots
+amount: 100, // MAX Amount of bots
 restartTimer : 30, // Restart Timeout in mins..  Set to 0 for no restart
 accNumber: 1,  // Which account to use
 regions: ["BR-Brazil", "CN-China", "EU-London", "JP-Tokyo", "RU-Russia", "SG-Singapore", "TK-Turkey", "US-Atlanta"],
@@ -13,8 +13,7 @@ accountsArr: [
 	{c_user: "YOUR_C_USER_3", datr: "YOUR_DATR_3", xs: "YOUR_XS_3"},
 ],
 tokenRefresh: 5, // Time to refresh fb token in mins
-//botsName: 'AGAR-XPBOT', // Bots name
-botsName: '﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽﷽௵', // Bots name
+botsName: 'AGAR-XPBOT', // Bots name
 debug: 0 // Set to 1 for debugging
 
 }
